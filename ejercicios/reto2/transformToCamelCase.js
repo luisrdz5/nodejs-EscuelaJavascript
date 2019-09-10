23eb2d6df089648e87e7d18303c50b46fa9145a8
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-09-09 19:55:14 -0500

    Se ha generado y configurado nuestro servidor express

--- ejercicios/reto2/transformToCamelCase.js
+++ ejercicios/reto2/transformToCamelCase.js
@@ -8,11 +8,9 @@ const transformStream = new Transform({
         cadena = chunk.toString().toLowerCase();
 
         let cadenaCC = cadena.split(' ').map(function(word,index){
-            // If it is the first word make sure to lowercase all the chars.
             if(index == 0){
               return word.toLowerCase();
             }
-            // If it is not the first word only upper case the first char and lowercase the rest.
             return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
           }).join('');
         this.push(cadenaCC.toString())
