2b19c75a157cedee441aca935ad545f9c429247d
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-09-11 21:17:16 -0500

    Se implemento la ruta y servicio PATCH

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -63,6 +63,23 @@ function moviesApi(app) {
       next(err);
     }
   });
+  router.patch('/:movieId', async function(req, res, next) {
+    const { movieId } = req.params;
+    const { body: movie } = req;
+    try {
+      const updatedMovieId = await moviesService.updateMovie({
+        movieId,
+        movie
+      });
+
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movie patched'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
   router.delete('/:movieId', async function(req, res, next) {
     const { movieId } = req.params;
     try {
