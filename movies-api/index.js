5f0b5d575a9cfec10cfab747f4aacd4bb6a317da
Luis Rodriguez <luis@luisrodriguez.com>
2019-09-12 22:41:14 -0500

    Se ha agregado un middleware que cacha los errores y los muestra de cierta forma en desarrollo y de otra forma en produccion

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,9 +4,14 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+
 app.use(express.json());
 
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
