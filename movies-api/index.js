ca866dd3152271202e28c88d67130bd59ccd19b5
Luis Rodriguez <luis@luisrodriguez.com>
2019-09-12 23:15:47 -0500

    Se ha implementado la libreria boom para el manejo de errores  http

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,13 +4,19 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers')
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 app.use(express.json());
 
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+//errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
