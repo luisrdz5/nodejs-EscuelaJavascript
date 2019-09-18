a2e7aa30b1e0a3a906188ffa91da5223b8cc6eb0
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-09-17 21:01:05 -0500

    Se han agregado los tests para nuestras rutas y para nuestros servicios

---
+++ movies-api/test/services.movies.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+
+const { MongoLibMock, getAllStub } = require('../utils/mocks/mongoLib');
+
+const { moviesMock } = require('../utils/mocks/movies');
+
+describe("services - movies", function() {
+    const MovieServices = proxyquire('../services/movies', {
+        '../lib/mongo': MongoLibMock
+    });
+
+    const moviesService = new MovieServices();
+    describe('when getMovies method is called', async function () {
+        it('should call the getAll MongoLib method', async function() {
+            await moviesService.getMovies({});
+            assert.strictEqual(getAllStub.called, true);
+        });
+        it('should return an array of Movies', async function () {
+            const result = await moviesService.getMovies({});
+            const expected = moviesMock;
+            assert.deepEqual(result, expected);
+        })
+    });
+});
