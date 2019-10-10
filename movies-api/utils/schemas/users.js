476d6f7c122e58eea33fa4341d63c229836e2c8a
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2019-10-10 13:42:34 -0500

    implemented the sign-up route and authorization

--- movies-api/utils/schemas/users.js
+++ movies-api/utils/schemas/users.js
@@ -3,7 +3,7 @@ const joi = require('@hapi/joi');
 const userIdSchema = joi.string().regex(/^[0-9a-f-A-F]{24}$/);
 
 const createUserSchema = {
-    nombre: joi.string().max(100).required(),
+    name: joi.string().max(100).required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
     isAdmin: joi.boolean()
